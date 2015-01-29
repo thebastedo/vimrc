d8c71588b50e0a8050a9828e851e5b59b9c75964
Justin Bastedo <justin.bastedo@ensighten.com>
2015-01-29 11:55:20 -0800

    Adding the setup.sh file, a package.json file (for jshint install) and adding npm-debug.log to the git ignore

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+echo 'Linking the vimrc...'
+ln -s ~/.vim/.vimrc ~/.vimrc
 
+echo 'Cloning Vundle vim plugin...'
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo 'Installing plugins...'
+vim +PluginInstall +qall
+
+echo 'Run npm install to install extra packages...'
+echo 'This will run with sudo so we can install jshint globally.'
+sudo npm install --unsafe-perm
